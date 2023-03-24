cd90c80a7abfd90e7e538dea9f264c279bc8859d
AlexandrM03 <alexandrmozolevskiy@gmail.com>
2023-03-24 21:29:20 +0300

    prisma added

--- Project/backend/src/main.ts
+++ Project/backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+
+	const prismaService = app.get(PrismaService);
+	await prismaService.enableShutdownHooks(app);
+
+	app.setGlobalPrefix('api');
+
+	await app.listen(4200);
 }
 bootstrap();
