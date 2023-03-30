59e5fdd2ccabb61af9c3c86880f358cc95db274c
AlexandrM03 <alexandrmozolevskiy@gmail.com>
2023-03-30 11:46:35 +0300

    postgresql creation script

--- Project/sql/main.sql
+++ Project/sql/main.sql
@@ -1,72 +1,72 @@
 create database main;
-use main;
 
 create table user_roles (
-    id int primary key identity,
+    id serial primary key,
     name varchar(50) not null
 );
 
 insert into user_roles (name) values ('admin'), ('user');
 
 create table users (
-    id int primary key identity,
+    id serial primary key,
     username varchar(50) not null unique,
     email varchar(50) not null unique,
     password varchar(300) not null,
-    active bit not null default 0,
-    role_id int foreign key references user_roles(id)
+    activation_code text,
+    role_id int default 2 references user_roles(id)
 );
 
-create table [statistics] (
-    id int primary key identity,
+create table statistics (
+    id serial primary key,
     tasks_completed int not null default 0,
     score int not null default 0,
-    user_id int foreign key references users(id)
+    user_id int references users(id)
 );
 
 create table task_complexities (
-    id int primary key identity,
+    id serial primary key,
     name varchar(50) not null
 );
 
 insert into task_complexities (name) values ('easy'), ('medium'), ('hard');
 
 create table tasks (
-    id int primary key identity,
+    id serial primary key,
     title varchar(50) not null,
-    description varchar(max) not null,
-    creation_time datetime not null default getdate(),
+    description text not null,
+    creation_time timestamp not null default timezone('utc-3', current_timestamp(0)),
     solved_times int not null default 0,
-    solution varchar(max) not null,
-    complexity_id int foreign key references task_complexities(id)
+    solution json not null,
+    accepted boolean not null default false,
+    complexity_id int references task_complexities(id)
 );
 
 create table solutions (
-    id int primary key identity,
-    query varchar(max) not null,
-    solution_time datetime not null default getdate(),
-    task_id int foreign key references tasks(id),
-    user_id int foreign key references users(id)
+    id serial primary key,
+    query text not null,
+    solution_time timestamp not null default timezone('utc-3', current_timestamp(0)),
+    task_id int references tasks(id),
+    user_id int references users(id)
 );
 
 create table comments (
-    id int primary key identity,
-    content varchar(max) not null,
-    creation_time datetime not null default getdate(),
-    user_id int foreign key references users(id),
-    task_id int foreign key references tasks(id)
+    id serial primary key,
+    content text not null,
+    creation_time timestamp not null default timezone('utc-3', current_timestamp(0)),
+    user_id int references users(id),
+    task_id int references tasks(id)
 );
 
 create table discussions (
-    id int primary key identity,
+    id serial primary key,
     topic varchar(50) not null
 );
 
 create table messages (
-    id int primary key identity,
-    content varchar(max) not null,
-    creation_time datetime not null default getdate(),
-    discussion_id int foreign key references discussions(id),
-    user_id int foreign key references users(id)
+    id serial primary key,
+    content text not null,
+    creation_time timestamp not null default timezone('utc-3', current_timestamp(0)),
+    discussion_id int references discussions(id),
+    user_id int references users(id)
 );
 
