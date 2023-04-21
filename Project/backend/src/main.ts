68a177f94eeada65de8fedad667d5f68b8ba37ae
AlexandrM03 <alexandrmozolevskiy@gmail.com>
2023-04-21 21:39:43 +0300

    swagger

--- Project/backend/src/main.ts
+++ Project/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -10,6 +11,14 @@ async function bootstrap() {
 
 	app.setGlobalPrefix('api');
 	app.enableCors();
+
+	const swaggerOptions = new DocumentBuilder()
+		.setTitle('API')
+		.setDescription('API description')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, swaggerOptions);
+	SwaggerModule.setup('swagger', app, document);
 
 	await app.listen(4200);
 }
