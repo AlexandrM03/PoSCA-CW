cb39a9b9c7bdc819fe07f42c4a9dd61c0e37230c
AlexandrM03 <alexandrmozolevskiy@gmail.com>
2023-04-21 15:23:19 +0300

    homepage and navigation

--- Project/frontend/src/app/app.module.ts
+++ Project/frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './layout/homepage/homepage.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomepageComponent,
+		NavigationComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatButtonModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
